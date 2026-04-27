72a78976f9537cbfdc7f3f42fc5a0794dbb57b38
Andrew Hicks <andrewhicks920@gmail.com>
2026-04-26 17:36:49 -0700

    Prod was down after a github repo change, so made some reference changes in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/hexagonal-chess/',
+  base: '/',
   test: {
     environment: 'node',
     typecheck: { tsconfig: './tsconfig.test.json' },
